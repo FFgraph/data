04b0f94b4bc6d72f3eb729a396612728792f74ac
Saurav Sharma <appdroiddeveloper@gmail.com>
2024-12-06 19:53:24 +0545

    refactor: direcly add data to sql file
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- global_options.sql
+++ global_options.sql
@@ -1,3 +1,23 @@
+-- Create global options table and generated fields
+CREATE TABLE global_options (
+    value JSONB NOT NULL,
+    name TEXT AS (jsonb_extract(value, '$.name')) STORED,
+    description TEXT AS (jsonb_extract(value, '$.description')) STORED,
+    type TEXT AS (jsonb_extract(value, '$.type')) STORED,
+    flag TEXT AS (jsonb_extract(value, '$.flag')) STORED
+);
+
+--- create unique index for name field
+CREATE UNIQUE INDEX global_options_name ON global_options(name);
+
+--- create index for flag field
+CREATE INDEX global_options_flag ON global_options(flag);
+
+--- load data which is in json format to global options
+INSERT INTO global_options (value)
+SELECT
+    jsonb_extract(value, '$')
+FROM json_each('
 [
     {
         "name": "repeat log",
@@ -74,3 +94,4 @@
         "support_no_prefix": true
     }
 ]
+');
